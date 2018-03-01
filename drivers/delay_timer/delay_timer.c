MarvellEmbeddedProcessors/atf-marvell

delay_timer: Add timer value get functions
Add ability to get timer value in micro- and milliseconds.

Change-Id: I864619c23fe8a2a01cef86795e39d1b0bfd2e3d3
Signed-off-by: Victor Axelrod <victora@marvell.com>
Signed-off-by: Ofir Fedida <ofedida@marvell.com>
Signed-off-by: Konstantin Porotchkin <kostap@marvell.com>
Reviewed-on: http://vgitil04.il.marvell.com:8080/37913
Reviewed-by: Haim Boot <hayim@marvell.com>

drivers/delay_timer/delay_timer.c
@@ -38,6 +38,29 @@
 static const timer_ops_t *ops;
 
 /***********************************************************
+ * Get a timer value in microseconds. The driver must
+ * be initialized before calling this function.
+ ***********************************************************/
+uint32_t timer_value_get(void)
+{
+	assert(ops != 0 &&
+		(ops->clk_mult != 0) &&
+		(ops->clk_div != 0) &&
+		(ops->get_timer_value != 0));
+
+	return (~ops->get_timer_value() * ops->clk_mult) / ops->clk_div;
+}
+
+/***********************************************************
+ * Get a timer value in miliseconds. The driver must
+ * be initialized before calling this function.
+ ***********************************************************/
+uint32_t timer_value_ms_get(void)
+{
+	return timer_value_get() / 1000;
+}
+
+/***********************************************************
  * Delay for the given number of microseconds. The driver must
  * be initialized before calling this function.
  ***********************************************************/

kostapr/arm-trusted-firmware

drivers/delay_timer/delay_timer.c
@@ -15,6 +15,30 @@
 static const timer_ops_t *timer_ops;
 
 /***********************************************************
+ * Get a timer value in microseconds. The driver must
+ * be initialized before calling this function.
+ ***********************************************************/
+uint32_t timer_value_get(void)
+{
+	assert(timer_ops != 0 &&
+		(timer_ops->clk_mult != 0) &&
+		(timer_ops->clk_div != 0) &&
+		(timer_ops->get_timer_value != 0));
+
+	return (~timer_ops->get_timer_value() *
+		timer_ops->clk_mult) / timer_ops->clk_div;
+}
+
+/***********************************************************
+ * Get a timer value in miliseconds. The driver must
+ * be initialized before calling this function.
+ ***********************************************************/
+uint32_t timer_value_ms_get(void)
+{
+	return timer_value_get() / 1000;
+}
+
+/***********************************************************
  * Delay for the given number of microseconds. The driver must
  * be initialized before calling this function.
  ***********************************************************/